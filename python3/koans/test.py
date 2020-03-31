c65f9469ac86873f209ea72c7c9f9163a84b2884
Myra Fah <FahMyra@JohnDeere.com>
2020-03-31 08:09:16 -0500

    completed more in about_control_statements

---
+++ python3/koans/test.py
@@ -0,0 +1,24 @@
+# def eval():
+#     i=1
+#     result = 1
+#     while i <= 10:
+#         result = result * i
+#         i += 1
+#         print(result)
+
+# def eval():
+#     i = 1
+#     result = 1
+#     while True:
+#         if i > 10: break
+#         result = result * i
+#         i += 1
+#         print(result)
+
+def eval():
+    i = 0
+    result = []
+    while i < 10:
+        i += 1
+        if (i % 2) == 0: continue
+        print(result.append(i))
